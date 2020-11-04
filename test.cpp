c50fbcb088642d934cb5930d89ad7168a6dc08e8
ShihFengLin <noahleft@gmail.com>
2020-11-04 20:40:40 +0800

    use sstream to test multiple MD5_Update

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <openssl/md5.h>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 std::string convert_md5(unsigned char* md5) {
     std::string retString = "";
@@ -25,6 +26,26 @@ BOOST_AUTO_TEST_CASE( md5 )
     MD5_CTX context;
     MD5_Init(&context);
     MD5_Update(&context, candidate, len);
+    MD5_Final(result2, &context);
+    BOOST_CHECK(convert_md5(result) == convert_md5(result2));
+}
+
+BOOST_AUTO_TEST_CASE( multi_md5 )
+{
+    std::stringstream ss;
+    unsigned char result[MD5_DIGEST_LENGTH];
+    ss << "Hello," << "World!";
+    MD5((unsigned char*)ss.str().c_str(), ss.str().size(), result);
+    ss.str(""); ss.clear();
+
+    unsigned char result2[MD5_DIGEST_LENGTH];
+    MD5_CTX context;
+    MD5_Init(&context);
+    ss << "Hello,";
+    MD5_Update(&context, (unsigned char*)ss.str().c_str(), ss.str().size());
+    ss.str(""); ss.clear();
+    ss << "World!";
+    MD5_Update(&context, (unsigned char*)ss.str().c_str(), ss.str().size());
     MD5_Final(result2, &context);
     BOOST_CHECK(convert_md5(result) == convert_md5(result2));
 }
